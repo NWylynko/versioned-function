806e03334b6243c7f2bd2737a1116b2174dac70e
Nick Wylynko <nick@wylynko.com>
2022-10-19 16:33:43 +0800

    validate the semver supplied

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
+import semverValid from "semver/functions/valid";
+
 export const versioned = <Functions extends { [key: string]: Function }>(functions: Functions) => {
   const versions = new Set<keyof Functions>();
 
   for (const version in functions) {
+
+    const validVersion = semverValid(version)
+
+    if (validVersion === null) {
+      throw new Error(`Version ${version} is not a valid semver`)
+    }
+
     versions.add(version);
   }
 
-  const matcher = <Version extends keyof Functions>(version: Version) => {
+  const matcher = <Version extends keyof Functions>(version: Version): Functions[Version] => {
     if (!versions.has(version)) {
       throw new Error(`The requested version does not exist`);
     }
